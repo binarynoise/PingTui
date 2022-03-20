95b0333b7a505a9c1658e23e1f96c5e28287ade9
binarynoise <binarynoise@mail.de>
2022-03-20 22:05:13 +0100

    skip build if already built

--- start.sh
+++ start.sh
@@ -2,4 +2,8 @@
 
 cd "$(dirname "$(realpath "$0")")" || exit 1
 
-./gradlew shadowJar && java -jar ./build/libs/PingTui-standalone.jar
+if [ ! -f ./build/libs/PingTui-standalone.jar ]; then
+  ./gradlew shadowJar || exit 1
+fi
+
+java -jar ./build/libs/PingTui-standalone.jar
