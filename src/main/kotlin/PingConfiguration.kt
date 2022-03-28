dd4e9859b902241481ea49448d332e3125f2ca4f
binarynoise <binarynoise@mail.de>
2022-03-28 12:53:50 +0200

    fix that automatic creation of the config file

--- src/main/kotlin/PingConfiguration.kt
+++ src/main/kotlin/PingConfiguration.kt
@@ -20,30 +20,34 @@ object PingConfiguration {
         
         val res: ResultWithDiagnostics<EvaluationResult> = evalFile(file)
         
-        if (res is ResultWithDiagnostics.Failure) {
+        if (res is ResultWithDiagnostics.Failure || !file.exists() || file.readText().isBlank()) {
             try {
-                file.bufferedWriter().write("""
-                package de.binarynoise.pingTui
-                
-                with(PingConfiguration) {
-                    
-                    hosts = listOf(
-                    
-                    )
-                    
-                    pingMethod = PingMethod.Tcp
-                    interval = 1000
-                    enableArpScan = true
-                    enableInterfaceScan = true
+                file.bufferedWriter().use {
+                    it.write("""
+                        package de.binarynoise.pingTui
+                        
+                        with(PingConfiguration) {
+                            
+                            hosts = listOf(
+                            
+                            )
+                            
+                            pingMethod = PingMethod.Tcp
+                            interval = 1000
+                            enableArpScan = true
+                            enableInterfaceScan = true
+                        }
+                        """.trimIndent())
+                    it.flush()
                 }
-                """.trimIndent())
+                assert(file.readText().isNotBlank())
                 System.err.println("""
-                    Konnte die config.properties nicht einlesen.
+                    Konnte die pingtui.config.kts nicht einlesen.
                     Die Datei wurde neu erstellt und mit den Standardwerten gefüllt.
                     """.trimIndent())
             } catch (e: Exception) {
                 System.err.println("""
-                    Konnte die config.properties nicht einlesen.
+                    Konnte die pingtui.config.kts nicht einlesen.
                     Die Datei konnte aber auch nicht neu erstellt und mit den Standardwerten gefüllt werden:
                     
                 """.trimIndent() + e.stackTraceToString())
