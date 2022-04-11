240a8ace94e9f6b31c065c68c2ce9b548a136030
binarynoise <binarynoise@mail.de>
2022-04-11 12:54:24 +0200

    fix/suppress deprecation in java 18 about Runtime.getRuntime().exec()

--- src/main/kotlin/ArpScanner.kt
+++ src/main/kotlin/ArpScanner.kt
@@ -13,20 +13,18 @@ interface ArpScanner {
     
     private object WindowsArpScanner : ArpScanner {
         override fun findNewHosts(): List<Host> {
-            return Runtime.getRuntime()
-                .exec("powershell Get-NetNeighbor -AddressFamily IPv4 -State Reachable | select -ExpandProperty IPAddress").inputStream.bufferedReader()
-                .lineSequence()
-                .map { Host(it) }
-                .toList()
+            val getNetNeighbor = "powershell Get-NetNeighbor -AddressFamily IPv4 -State Reachable | select -ExpandProperty IPAddress"
+            return getNetNeighbor.exec().map { Host(it) }.toList()
         }
     }
     
     private object LinuxArpScanner : ArpScanner {
         override fun findNewHosts(): List<Host> {
-            return Runtime.getRuntime().exec("ip -4 neigh show nud reachable").inputStream.bufferedReader()
-                .lineSequence()
-                .map { Host(it.substringBefore(' ')) }
-                .toList()
+            val ipNeigh = "ip -4 neigh show nud reachable"
+            return ipNeigh.exec().map { Host(it.substringBefore(' ')) }.toList()
         }
     }
 }
+
+@Suppress("DEPRECATION")
+fun String.exec() = Runtime.getRuntime().exec(this).inputStream.bufferedReader().lineSequence()
